892af6475089732c530d4fc1a56534de28c3b1c9
Your Name <your.email@example.com>
2026-06-04 11:21:18 +0530

    Add Day 4 folder with solutions

---
+++ Week1/Day 4/q1.cpp
@@ -0,0 +1,23 @@
+/*
+ Problem: Diagonal Sum in a Matrix(LC 1572)
+ Approach:Sum matrix elements where i == j or i + j == n - 1.
+ Difficulty: Easy
+ Time Complexity: O(m*n)
+ Concept: Matrix Traversal
+ */
+
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+        int sum = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j || i + j == n - 1)
+                    sum += mat[i][j];
+            }
+        }
+        return sum;
+    }
+};
---
+++ Week1/Day 4/q2.cpp
@@ -0,0 +1,30 @@
+/* 
+Problem: Reshape the Matrix(LC 566)
+Approach: Simulation
+Difficulty: Easy
+Time Complexity: O(m*n)
+Concept: Validate `m*n == r*c`, then sequentially copy elements into the new `r x c` matrix. 
+*/
+
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        vector<vector<int>> ans(r, vector<int>(c));
+        int m = mat.size();
+        int n = mat[0].size();
+        if ((m * n) != (r * c))
+            return mat;
+        int row = 0, col = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[row][col] = mat[i][j];
+                col++;
+                if (col == c) {
+                    row++;
+                    col = 0;
+                }
+            }
+        }
+        return ans;
+    }
+};
